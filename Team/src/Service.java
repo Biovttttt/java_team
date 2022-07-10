43e47abdeeeadbf3188fb99cd1f602bcf8448c9b
Biovttttt <biovttttt@gmail.com>
2022-07-10 17:05:42 +0600

    Создал классы

---
+++ Team/src/Service.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public interface Service {
+
+
+    void changeDriver(List<JsonCarInfo> jsonCarInfo, List<Driver> driver, int id_truck, int id_driver);
+    void startDriving(List<JsonCarInfo> jsonCarInfo, int id_truck);
+    void startRepair(List<JsonCarInfo> jsonCarInfo, int id_truck);
+
+}
---
+++ Team/src/Status.java
@@ -0,0 +1,5 @@
+public enum Status {
+    BASE,
+    ROUTE,
+    REPAIR
+}
